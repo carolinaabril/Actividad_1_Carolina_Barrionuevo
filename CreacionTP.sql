0dce2fffbd03920aa66ec6e57a083ecde5183717
carolinaabril <cbarrionuevo@uade.edu.ar>
2025-10-25 15:12:26 -0300

    tablas completas e inserts hechos para todas las tablas EXCEPTO LAS NUEVAS(cuatrimestre,matriculacion, cuota, etc)

--- CreacionTP.sql
+++ CreacionTP.sql
@@ -52,25 +52,41 @@ CREATE TABLE INSCRIPCIONES( --referida
     fecha_inscripcion DATE NOT NULL, 
     nota_teorica DECIMAL(4,2) NOT NULL, --cambiar nombre a nota_teorica_1
         CONSTRAINT chk_nota_teorica CHECK (nota_teorica BETWEEN 1 AND 10),
-    --nota_teorica_2 decimal(4,2) not null,
-    --    constraint chk_nota_teorica2 check (nota_teorica_2 between 1 and 10)
-    nota_practica DECIMAL(4,2) NOT NULL,
-        CONSTRAINT chk_nota_practica CHECK (nota_practica BETWEEN 1 AND 10),
-    --nota_teorica_recuperatorio decimal(4,2) null,
+    nota_practica decimal(4,2) null,
+    CONSTRAINT chk_nota_practica CHECK (nota_practica BETWEEN 1 AND 10),
     nota_final AS ((nota_teorica + nota_practica)/2.0),  -- columna calculada
-
+    
     CONSTRAINT pk_estudianteInscripto PRIMARY KEY(id_estudiante, id_curso),
     CONSTRAINT fk_estudiante FOREIGN KEY (id_estudiante) REFERENCES ESTUDIANTES(id_estudiante),
     CONSTRAINT fk_curso FOREIGN KEY (id_curso) REFERENCES CURSOS(id_curso)
 );
-EXEC sp_rename 'INSCRIPCIONES.nota_teorica', 'nota_teorica_1', 'COLUMN';
-ALTER TABLE INSCRIPCIONES 
-DROP CONSTRAINT chk_nota_teorica;
+
+ALTER TABLE INSCRIPCIONES DROP CONSTRAINT chk_nota_teorica; --BORRAMOS CONSTRAINT
+ALTER TABLE INSCRIPCIONES DROP COLUMN nota_final; --BORRAMOS NOTA FINAL PORQUE LA CALCULAMOS CON NOTA_TEORICA
+GO
+
+EXEC sp_rename 'INSCRIPCIONES.nota_teorica', 'nota_teorica_1', 'COLUMN'; --renombramos nota_teorica como nota_teorica_1
+GO
 
 ALTER TABLE INSCRIPCIONES
 ADD CONSTRAINT chk_nota_teorica_1 CHECK (nota_teorica_1 BETWEEN 1 AND 10);
+GO
 
+ALTER TABLE INSCRIPCIONES
+ADD nota_teorica_2 decimal(4,2) null,
+constraint chk_nota_teorica_2 check (nota_teorica_2 between 1 and 10);
+GO
 
+ALTER TABLE INSCRIPCIONES
+ADD nota_teorica_recuperatorio decimal(4,2) null,
+ constraint chk_nota_teorica_recuperatorio check (nota_teorica_recuperatorio between 1 and 10);
+GO
+ --nota final seria nota_teorica_1 + nota_teorica_2 + nota_practica + recuperatorio (si aplica) / 3 o 4
+ALTER TABLE INSCRIPCIONES
+ADD nota_final decimal(4,2) null,
+constraint chk_nota_final check (nota_final between 1 and 10);
+
+--TABLAS NUEVAS
 CREATE TABLE CUATRIMESTRE(
     id_cuatrimestre int not null,
     nombre varchar(100) not null,
@@ -81,9 +97,11 @@ CREATE TABLE CUATRIMESTRE(
 );
 
 create table ESTADOS_PAGO(
-id_estado_pago INT,
-    nombre_estado VARCHAR(20) NOT NULL 
+id_estado_pago INT not null,
+nombre_estado VARCHAR(20) NOT NULL,
+    CONSTRAINT pk_id_estado_pago primary key(id_estado_pago)
 );
+
 INSERT INTO ESTADOS_PAGO (id_estado_pago, nombre_estado)
 VALUES (1, 'PENDIENTE'),
        (2, 'PAGA'),
@@ -93,7 +111,7 @@ create table factura(
 id_factura int not null,
 id_estudiante int not null,
 mes int not null,
-constraint chk_mes check (mes between 1 and 12), 
+constraint chk_mes_factura check (mes between 1 and 12), 
 anio int not null,
 fecha_emision date not null,
 fecha_vencimiento date not null,
@@ -112,14 +130,14 @@ id_estudiante int not null,
 id_cuatrimestre int not null,
 id_factura int not null,
 mes int not null,
-constraint chk_mes check (mes between 1 and 12),
+constraint chk_mes_cuota check (mes between 1 and 12),
 monto decimal(6,2) not null,
 constraint chk_monto_cuota check(monto > 0),
 fecha_vencimiento date not null,
 id_estado_pago int not null,
-constraint fk_estado_pago foreign key (id_estado_pago) references ESTADOS_PAGO(id_estado_pago),
+constraint fk_cuota_estado_pago foreign key (id_estado_pago) references ESTADOS_PAGO(id_estado_pago),
 constraint pk_cuota primary key(id_cuota),  
-constraint fk_id_estudiante foreign key (id_estudiante) references estudiantes(id_estudiante),
+constraint fk_id_estudiante_cuota foreign key (id_estudiante) references estudiantes(id_estudiante),
 constraint fk_id_cuatrimestre foreign key (id_cuatrimestre) references cuatrimestre(id_cuatrimestre),
 constraint fk_id_factura foreign key(id_factura) references factura(id_factura)
 );
@@ -132,10 +150,10 @@ fecha_pago date not null,
 monto decimal(6,2) not null,
 constraint chk_monto_matriculacion check(monto > 0),
 id_estado_pago int not null,
-constraint fk_estado_pago foreign key (id_estado_pago) references ESTADOS_PAGO(id_estado_pago),
+constraint fk_matriculacion_estado_pago foreign key (id_estado_pago) references ESTADOS_PAGO(id_estado_pago),
 
 constraint pk_id_matricula primary key(id_matricula),
-constraint fk_id_estudiante foreign key (id_estudiante) references estudiantes(id_estudiante)
+constraint fk_id_estudiante_matricula foreign key (id_estudiante) references estudiantes(id_estudiante)
 );
 
 create table CUENTACORRIENTE(
@@ -145,11 +163,11 @@ fecha date not null,
 concepto varchar(100) not null,
 monto decimal(6,2) not null,
 constraint chk_monto_cuenta_corriente check(monto > 0),
-estado varchar(15) not null, --que estados puede tener la cuenta corriente?
-constraint chk_estado check (estado in ('PENDIENTE','PAGA','VENCIDA')),
+id_estado_pago int not null, 
+constraint fk_cuentacorriente_estado_pago foreign key (id_estado_pago) references ESTADOS_PAGO(id_estado_pago),
 
 CONSTRAINT pk_id_movimiento primary key(id_movimiento),
-constraint fk_id_estudiante foreign key (id_estudiante) references estudiantes(id_estudiante)
+constraint fk_id_estudiante_cuentacorriente foreign key (id_estudiante) references estudiantes(id_estudiante)
 );
 
 create table INTERESPORMORA(
@@ -164,8 +182,6 @@ CREATE TABLE itemFactura (
     id_factura INT NOT NULL,
     id_curso INT NOT NULL,
     PRIMARY KEY (id_factura, id_curso),
-    CONSTRAINT FK_ItemFactura_Factura FOREIGN KEY (id_factura) REFERENCES Factura(id_factura),
-    CONSTRAINT FK_ItemFactura_Curso FOREIGN KEY (id_curso) REFERENCES Cursos(id_curso)
+    CONSTRAINT FK_itemFactura_Factura FOREIGN KEY (id_factura) REFERENCES Factura(id_factura),
+    CONSTRAINT FK_itemFactura_Curso FOREIGN KEY (id_curso) REFERENCES Cursos(id_curso)
 );
-
---prueba  
--- Insert.sql
+++ Insert.sql
@@ -46,6 +46,15 @@ values
 (300,	'Carla',	'Diaz',	'carla@example.com'),
 (400,	'Mateo',	'Suarez',	'mateo@example.com'),
 (500,	'Sofia',	'Torres',	'sofia@example.com');
+
+--agregamos año ingreso
+UPDATE ESTUDIANTES SET anio_ingreso = 2023 WHERE id_estudiante = 100; 
+UPDATE ESTUDIANTES SET anio_ingreso = 2023 WHERE id_estudiante = 200; 
+UPDATE ESTUDIANTES SET anio_ingreso = 2024 WHERE id_estudiante = 300; 
+UPDATE ESTUDIANTES SET anio_ingreso = 2025 WHERE id_estudiante = 400; 
+UPDATE ESTUDIANTES SET anio_ingreso = 2025 WHERE id_estudiante = 500; 
+UPDATE ESTUDIANTES SET anio_ingreso = 2025 WHERE id_estudiante = 600; 
+
 --cursos
 insert into CURSOS	(id_curso,nombre_curso,descripcion,anio,id_profesor,id_materia)
 values
@@ -62,11 +71,11 @@ VALUES
 (9106,'Curso Matemática I Noche','Noctuno',2025,1111,501);
 
 --inscripciones
-insert into INSCRIPCIONES(id_estudiante,id_curso,fecha_inscripcion,nota_teorica,nota_practica)
+insert into INSCRIPCIONES(id_estudiante,id_curso,fecha_inscripcion,nota_teorica_1,nota_practica)
 values
 
 (100,	9001,	'2023-05-10',	7,	9),
-(100,	9002,	'2024-06-20',	9,	8),
+(100, 	9002,	'2024-06-20',	9,	8),
 (200,	9001,	'2023-07-01',	5,	6),
 (200,	9003,	'2025-03-12',	8,	9),
 (300,	9002,	'2024-04-15',	4,	6),
@@ -76,12 +85,104 @@ values
 (500,	9005,	'2025-07-01',	2,	4),
 (500,	9004,	'2025-07-10',	10,	10);
 
-insert into INSCRIPCIONES(id_estudiante,id_curso,fecha_inscripcion,nota_teorica,nota_practica)
+insert into INSCRIPCIONES(id_estudiante,id_curso,fecha_inscripcion,nota_teorica_1,nota_practica)
 values
 (600,9004,'2025-06-12',10,10),
 (100,9004,'2024-05-13',10,10),
 (200,9004,'2025-07-14',10,10),
 (400,9005, '2023-10-01', 8, 9); 
 
+--modificaciones a inscripciones
 
---prueba
+-- Estudiante 100
+UPDATE INSCRIPCIONES SET nota_teorica_2 = 8 WHERE id_estudiante = 100 AND id_curso = 9001;
+UPDATE INSCRIPCIONES SET nota_teorica_2 = 9 WHERE id_estudiante = 100 AND id_curso = 9002;
+UPDATE INSCRIPCIONES SET nota_teorica_2 = 10 WHERE id_estudiante = 100 AND id_curso = 9004;
+-- Estudiante 200
+UPDATE INSCRIPCIONES SET nota_teorica_2 = 6 WHERE id_estudiante = 200 AND id_curso = 9001;
+UPDATE INSCRIPCIONES SET nota_teorica_2 = 9 WHERE id_estudiante = 200 AND id_curso = 9003;
+UPDATE INSCRIPCIONES SET nota_teorica_2 = 10 WHERE id_estudiante = 200 AND id_curso = 9004;
+-- Estudiante 300
+UPDATE INSCRIPCIONES SET nota_teorica_2 = 7 WHERE id_estudiante = 300 AND id_curso = 9002;
+UPDATE INSCRIPCIONES SET nota_teorica_2 = 9 WHERE id_estudiante = 300 AND id_curso = 9004;
+-- Estudiante 400
+UPDATE INSCRIPCIONES SET nota_teorica_2 = 8 WHERE id_estudiante = 400 AND id_curso = 9003;
+UPDATE INSCRIPCIONES SET nota_teorica_2 = 5 WHERE id_estudiante = 400 AND id_curso = 9004;
+UPDATE INSCRIPCIONES SET nota_teorica_2 = 9 WHERE id_estudiante = 400 AND id_curso = 9005;
+-- Estudiante 500
+UPDATE INSCRIPCIONES SET nota_teorica_2 = 3 WHERE id_estudiante = 500 AND id_curso = 9005;
+UPDATE INSCRIPCIONES SET nota_teorica_2 = 10 WHERE id_estudiante = 500 AND id_curso = 9004;
+-- Estudiante 600
+UPDATE INSCRIPCIONES SET nota_teorica_2 = 9 WHERE id_estudiante = 600 AND id_curso = 9004;
+
+--recuperatorios
+UPDATE INSCRIPCIONES
+SET nota_teorica_recuperatorio = 7
+WHERE id_estudiante = 200 AND id_curso = 9001;
+
+UPDATE INSCRIPCIONES
+SET nota_teorica_recuperatorio = 6
+WHERE id_estudiante = 300 AND id_curso = 9002;
+
+UPDATE INSCRIPCIONES
+SET nota_teorica_recuperatorio = 8
+WHERE id_estudiante = 400 AND id_curso = 9004;
+
+UPDATE INSCRIPCIONES
+SET nota_teorica_recuperatorio = 7
+WHERE id_estudiante = 500 AND id_curso = 9005;
+
+-- nota final de alumnos SIN recuperatorio
+UPDATE INSCRIPCIONES
+SET nota_final = (nota_teorica_1 + nota_teorica_2 + nota_practica) / 3.0
+WHERE nota_teorica_recuperatorio IS NULL;
+
+-- nota final de alumnos CON recuperatorio
+UPDATE INSCRIPCIONES
+SET nota_final = (nota_teorica_1 + nota_teorica_2 + nota_practica + nota_teorica_recuperatorio) / 4.0
+WHERE nota_teorica_recuperatorio IS NOT NULL;
+
+--TODAVIA NO INSERTE TODO ESTO!!!!!!
+--nuevas tablas
+INSERT INTO CUATRIMESTRE(id_cuatrimestre, nombre, fecha_inicio, fecha_fin)
+VALUES 
+(1,'Cuatrimestre 1 2025','2025-03-01','2025-06-30'),
+(2,'Cuatrimestre 2 2025','2025-07-01','2025-10-31');
+
+INSERT INTO MATRICULACION(id_matricula, id_estudiante, anio, fecha_pago, monto, id_estado_pago)
+VALUES
+(1,100,2025,'2025-03-01',5000,2),
+(2,200,2025,'2025-03-02',5000,2);
+
+
+-- factura
+INSERT INTO FACTURA(id_factura, id_estudiante, mes, anio, fecha_emision, fecha_vencimiento, monto_total, id_estado_pago)
+VALUES
+(1,100,3,2025,'2025-03-01','2025-03-15',1000,1),
+(2,200,3,2025,'2025-03-01','2025-03-15',1000,1);
+
+-- cuotas
+INSERT INTO CUOTA(id_cuota, id_estudiante, id_cuatrimestre, id_factura, mes, monto, fecha_vencimiento, id_estado_pago)
+VALUES
+(1,100,1,1,3,1000,'2025-03-15',1),
+(2,200,1,2,3,1000,'2025-03-15',1);
+
+-- item factura
+INSERT INTO ITEMFACTURA(id_factura, id_curso)
+VALUES
+(1,9001),
+(2,9003);
+
+INSERT INTO CUENTACORRIENTE(id_movimiento, id_estudiante, fecha, concepto, monto, id_estado_pago)
+VALUES
+(1,100,'2025-03-01','Matrícula',5000,2),
+(2,100,'2025-03-01','Cuota marzo',1000,1),
+(3,200,'2025-03-01','Matrícula',5000,2),
+(4,200,'2025-03-01','Cuota marzo',1000,1);
+
+INSERT INTO INTERESPORMORA(anio_carrera, porcentaje_interes)
+VALUES
+(1,2.5),
+(2,2.0),
+(3,1.5);
+
