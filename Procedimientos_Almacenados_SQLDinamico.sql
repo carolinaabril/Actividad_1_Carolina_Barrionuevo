8cf37b06de92671ec5d2934dfa46bf9d762d5b4a
nellalopez13 <antonella.lopezb@gmail.com>
2025-10-31 16:54:30 -0300

    ProcedimientosAlmacenados con SQL dinamico (falta)

--- Procedimientos_Almacenados_SQLDinamico.sql
+++ Procedimientos_Almacenados_SQLDinamico.sql
@@ -80,10 +80,32 @@ exec CursosXInscriptos
 	@min_inscriptos=5
 
 -- 4.	Generar un reporte de facturas agrupadas por un campo dinámico (mes, estado_pago, estudiante). 
+
+create procedure ReporteFacturasAgrupacion 
+	@campo nvarchar (50)
+
+as
+begin
+	declare @sql nvarchar(max)
+	declare  @selectCampo nvarchar(100)
+
+	if @campo = 'mes'
+		set @selectCampo = 'month(fecha)'
+	else
+		set @selectCampo = QUOTENAME (@campo)
+
+	set @sql = '
+		select ' + @selectCampo + ' AS agrupado_por, sum(monto) AS total_facturado, count (*) AS cantidad_movimientos
+		from CUENTACORRIENTE
+		group by ' + @selectCampo + ' 
+		order by ' + @selectCampo
+
+	exec sp_executesql @sql
+end
+
 -- 5.	Listar cuotas vencidas ordenadas por un campo dinámico (fecha_vencimiento, monto, estado_pago). 
 -- 6.	Mostrar los cursos que cumplen con una condición dinámica (por ejemplo, costo mensual > X, créditos < Y, año = Z). 
 -- 7.	Listar profesores que dictan cursos en un cuatrimestre específico, con posibilidad de ordenar por nombre, apellido o especialidad. 
 -- 8.	Consultar movimientos de cuenta corriente filtrando por múltiples conceptos seleccionados por el usuario (por ejemplo, 'matrícula', 'cuota', 'interés'). 
 -- 9.	Listar inscripciones donde el usuario define qué columnas mostrar (por ejemplo, nota_final, nota_teorica_1, nota_practica). 
 -- 10.	Generar un listado de estudiantes con filtros dinámicos combinados (por ejemplo, año_ingreso > X AND apellido LIKE '%Y%'). 
-
